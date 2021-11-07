f8c1b0948923eaae6ef480d904f7e07e4cd949a9
Kurd <h.kurd@vasl.ir>
2021-11-07 18:05:59 +0330

    Persian Calendar

---
+++ app/src/main/java/com/hosseinkurd/app/persiancalendar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hosseinkurd.app.persiancalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
